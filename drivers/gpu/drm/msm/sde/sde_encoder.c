crdroidandroid/android_kernel_xiaomi_sm6150

drm: Import minimal Xiaomi changes for G7B 120 HZ panel.
Change-Id: I6e86500090b3f5547b1882acc0f92ca24f02acaa

drivers/gpu/drm/msm/sde/sde_encoder.c
@@ -3735,6 +3735,7 @@ static void sde_encoder_get_qsync_fps_callback(
 	sde_enc = to_sde_encoder_virt(drm_enc);
 	disp_info = &sde_enc->disp_info;
 	*qsync_fps = disp_info->qsync_min_fps;
+	pr_info("[%s] the qsync min fps is %d, disp_info is %p",__func__, disp_info->qsync_min_fps, disp_info);
 }
 
 int sde_encoder_idle_request(struct drm_encoder *drm_enc)
@@ -5459,6 +5460,7 @@ static const struct drm_encoder_funcs sde_encoder_funcs = {
 		.early_unregister = sde_encoder_early_unregister,
 };
 
+struct msm_display_info *msm_display_info;
 struct drm_encoder *sde_encoder_init_with_ops(
 		struct drm_device *dev,
 		struct msm_display_info *disp_info,
@@ -5472,6 +5474,7 @@ struct drm_encoder *sde_encoder_init_with_ops(
 	char name[SDE_NAME_SIZE];
 	int ret = 0, i, intf_index = INTF_MAX;
 	struct sde_encoder_phys *phys = NULL;
+	static int j = 0;
 
 	sde_enc = kzalloc(sizeof(*sde_enc), GFP_KERNEL);
 	if (!sde_enc) {
@@ -5540,6 +5543,12 @@ struct drm_encoder *sde_encoder_init_with_ops(
 			sde_encoder_esd_trigger_work_handler);
 
 	memcpy(&sde_enc->disp_info, disp_info, sizeof(*disp_info));
+
+	if(j == 0) {
+		msm_display_info = &sde_enc->disp_info;
+	}
+	j++;
+	pr_debug("[%s] g_msm_display_info is %p",__func__, msm_display_info);
 
 	SDE_DEBUG_ENC(sde_enc, "created\n");
 

Project-Fluid-Devices/kernel_xiaomi_sm6150

drivers/gpu/drm/msm/sde/sde_encoder.c
@@ -3731,6 +3731,7 @@ static void sde_encoder_get_qsync_fps_callback(
 	sde_enc = to_sde_encoder_virt(drm_enc);
 	disp_info = &sde_enc->disp_info;
 	*qsync_fps = disp_info->qsync_min_fps;
+	pr_debug("[%s] the qsync min fps is %d, disp_info is %p",__func__, disp_info->qsync_min_fps, disp_info);
 }
 
 int sde_encoder_idle_request(struct drm_encoder *drm_enc)
@@ -5455,6 +5456,7 @@ static const struct drm_encoder_funcs sde_encoder_funcs = {
 		.early_unregister = sde_encoder_early_unregister,
 };
 
+struct msm_display_info *msm_display_info;
 struct drm_encoder *sde_encoder_init_with_ops(
 		struct drm_device *dev,
 		struct msm_display_info *disp_info,
@@ -5468,6 +5470,7 @@ struct drm_encoder *sde_encoder_init_with_ops(
 	char name[SDE_NAME_SIZE];
 	int ret = 0, i, intf_index = INTF_MAX;
 	struct sde_encoder_phys *phys = NULL;
+	static int j = 0;
 
 	sde_enc = kzalloc(sizeof(*sde_enc), GFP_KERNEL);
 	if (!sde_enc) {
@@ -5536,6 +5539,12 @@ struct drm_encoder *sde_encoder_init_with_ops(
 			sde_encoder_esd_trigger_work_handler);
 
 	memcpy(&sde_enc->disp_info, disp_info, sizeof(*disp_info));
+
+	if(j == 0) {
+		msm_display_info = &sde_enc->disp_info;
+	}
+	j++;
+	pr_debug("[%s] g_msm_display_info is %p",__func__, msm_display_info);
 
 	SDE_DEBUG_ENC(sde_enc, "created\n");
 